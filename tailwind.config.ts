e1b4f659ff8424c8246906c06f7ed1187c876661
Cicero Haryono <gba.cicero.haryono@gmail.com>
2025-06-07 13:24:55 +0000

    change the font theme to the font tangerine

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,9 +11,9 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"Baloo 2"', 'sans-serif'],
-        body: ['"Baloo 2"', 'sans-serif'],
-        headline: ['"Baloo 2"', 'sans-serif'],
+        sans: ['Tangerine', 'cursive', 'sans-serif'],
+        body: ['Tangerine', 'cursive', 'sans-serif'],
+        headline: ['Tangerine', 'cursive', 'sans-serif'],
       },
       colors: {
         background: 'hsl(var(--background))',
